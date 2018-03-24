ff5644c612fbae16dbffb99054bb218fe3b78017
Chintan <chintan@wohlig.com>
2018-03-24 19:05:32 +0530

    ok

--- api/controllers/MarketsController.js
+++ api/controllers/MarketsController.js
@@ -1,3 +1,15 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+
+    check: function (req, res) {
+        var ccxt = require('ccxt');
+        res.json(ccxt);
+        var binance = new ccxt.binance();
+        var hitbtc = new ccxt.hitbtc();
+
+
+    }
+
+
+};
 module.exports = _.assign(module.exports, controller);
