1cd73d558bffa610a49a43272b1ebab99a893d73
Chintan <chintan@wohlig.com>
2018-03-25 10:58:11 +0530

    changes

--- api/controllers/MarketsController.js
+++ api/controllers/MarketsController.js
@@ -2,43 +2,58 @@ module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
 
     check: function (req, res) {
-        var symbol = req.body.symbol;
-
         var binance = new ccxt.binance();
         var hitbtc = new ccxt.hitbtc2();
         var costInCommission = 0.2;
-
         async.waterfall([
             function (callback) {
                 async.parallel({
                     binance: function (callback) {
                         binance.loadMarkets().then(function (data) {
                             callback();
+                        }).catch(function (err) {
+                            callback(err);
                         });
                     },
                     hitbtc: function (callback) {
                         hitbtc.loadMarkets().then(function (data) {
                             callback();
+                        }).catch(function (err) {
+                            callback(err);
                         });
                     }
                 }, callback);
             },
             function (data, callback) {
-                async.parallel({
-                    binanceData: function (callback) {
-                        binance.fetchOHLCV(symbol, '1m').then(function (data) {
-                            callback(null, Markets.convertOHLCV(data));
-                        });
-                    },
-                    hitbtcData: function (callback) {
-                        hitbtc.fetchOHLCV(symbol, '1m').then(function (data) {
-                            callback(null, Markets.convertOHLCV(data));
-                        });
-                    }
-                }, callback);
+                callback(null, _.intersection(binance.symbols, hitbtc.symbols));
             },
-            function (data, callback) {
-                callback(null, Markets.convert2MarketsData(data.binanceData, data.hitbtcData, costInCommission));
+            function (unionSymbols, callback) {
+                console.log(unionSymbols[0]);
+                async.concatSeries([unionSymbols[0]], function (symbol, callback) {
+                    async.parallel({
+                        binanceData: function (callback) {
+                            binance.fetchOHLCV(symbol, '1m').then(function (data) {
+                                callback(null, Markets.convertOHLCV(data));
+                            }).catch(function (err) {
+                                callback(err);
+                            });
+                        },
+                        hitbtcData: function (callback) {
+                            hitbtc.fetchOHLCV(symbol, '1m').then(function (data) {
+                                callback(null, Markets.convertOHLCV(data));
+                            }).catch(function (err) {
+                                callback(err);
+                            });
+                        }
+                    }, callback);
+                }, callback);
+
+            },
+            function (allSymbolsData, callback) {
+                var returnData = _.map(allSymbolsData, function (data) {
+                    return Markets.convert2MarketsData(data.binanceData, data.hitbtcData, costInCommission);
+                });
+                callback(null, returnData);
             }
         ], res.callback);
 
@@ -54,7 +69,6 @@ var controller = {
         exchange.loadMarkets().then(function (data) {
             res.json(exchange.symbols);
         });
-        console.log();
     },
 
 
